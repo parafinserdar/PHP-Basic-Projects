00242f8999824c473741891e84aaeeb109fa0819
Parafin <parafinserdar@gmail.com>
2024-03-22 04:00:07 +0300

    sorunlar giderildi.

---
+++ Project-1/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kurs Uygulaması</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css"/>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-primary navbar dark">
+        <div class="container">
+        <a href="/index.php" class="navbar-brand">Kurs Uygulaması</a> 
+       <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+            <a href="" class="nav-link">Anasayfa</a>
+        </li>
+        <li class="nav-item">
+            <a href="" class="nav-link">Kurslar</a>
+        </li>
+       </ul> 
+        </div>
+    </nav>
+
+<div class="container my-3">
+    <div class="row">
+        <div class="col-3">
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action">Programlama Kategorisi</a>
+                <a href="#" class="list-group-item list-group-item-action">Web Geliştirme</a>
+                <a href="#" class="list-group-item list-group-item-action">Mobil Uygulama</a>
+            </div>
+        </div>
+        <div class="col-9">
+            <div class="card mb-3">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="image/1.jpg" class ="img-fluid rounded-start" alt="resim1">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">Web Geliştirme</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia consequatur mollitia incidunt atque provident repudiandae quam. Aliquam neque labore quas.</p>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="image/2.jpg" class ="img-fluid rounded-start" alt="resim2">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">Python Kursu</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia consequatur mollitia incidunt atque provident repudiandae quam. Aliquam neque labore quas.</p>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="image/3.jpg" class ="img-fluid rounded-start" alt="resim3">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">Javascript Kursu</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia consequatur mollitia incidunt atque provident repudiandae quam. Aliquam neque labore quas.</p>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+
+</body>
+</html>
